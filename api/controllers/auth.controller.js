94a7e3e542d198b80e56d874187aa97367af5dcd
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-04-10 17:46:15 +0530

    added middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,32 +1,35 @@
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req,res) => {
-    const {username,email,password}=req.body;
-    if (!username ||!email ||!password || username==="" || email==="" || password==="") {
-        return res.status(400).json({
-            message: "Please enter all fields"
-        })
-    }
+export const signup = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
 
-    const hashedPassword = bcryptjs.hashSync(password,10);
+        // Check if all fields are provided
+        if (!username || !email || !password || username === "" || email === "" || password === "") {
+            return next(errorHandler(400, "Please enter all fields"));
+        }
 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword
-    });
+        // Hash the password
+        const hashedPassword = await bcryptjs.hash(password, 10);
 
-    try{
+        // Create a new user instance
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        // Save the user to the database
         await newUser.save();
+
+        // Respond with success message
         res.json({
             message: "User created successfully"
-        })
-
-    } catch(err){
-        res.status(500).json({
-            message: err.message
-        })
+        });
+    } catch (error) {
+        // Pass the error to the error handler middleware
+        next(error);
     }
-
-}
+};
