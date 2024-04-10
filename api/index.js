e9b344b42615c5d1dfad8f4d0d9f55eb4337b6c7
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-04-10 11:06:03 +0530

    Added controllers and routes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
+import userRoutes from './routes/user.route.js';
 
 mongoose.connect("mongodb+srv://actspot:actspot2024@mern-blog.m526lr4.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog").then(()=>{
     console.log("Connected to MongoDB");
@@ -12,3 +13,5 @@ const app=express();
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
+
+app.use('/api/user',userRoutes)
